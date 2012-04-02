7af639efbb426cb84cd22afb841af814c5ecf307
Bjørn Bäuchle <baeuchle@th.physik.uni-frankfurt.de>
2012-04-02 08:57:57 +0200

    Netzpläne April2012, Countries: 28

---
+++ entscripten.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl -w
+
+$script = 0;
+$g_ausbau = 0;
+$g_klick = 0;
+$g_kilo = 0;
+
+while( <> ) {
+ $script = 1 if /<script\b/;
+ s/onclick="[^"]"//g;
+ 
+ $g_ausbau++ if /<g/ && $g_ausbau > 0;
+ $g_ausbau++  if /<g id="ausbauprojekte"/;
+
+ $g_klick++ if /<g/ && $g_klick > 0;
+ $g_klick++  if /<g id="clickrechtecke"/;
+
+ $g_kilo++ if /<g/ && $g_kilo > 0;
+ $g_kilo++  if /<g id="kilometrierungen"/;
+
+ print unless $script > 0 || $g_ausbau > 0 || $g_kilo > 0 || $g_klick > 0;
+ $script = 0 if /<\/script>/;
+ $g_kilo-- if /<\/g/ && $g_kilo > 0;
+ $g_klick-- if /<\/g/ && $g_klick > 0;
+ $g_ausbau-- if /<\/g/ && $g_ausbau > 0;
+}
