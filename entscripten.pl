5301d976cf1d4ec80c03851ef7aa5a642d49564f
Bjørn Bäuchle <baeuchle@th.physik.uni-frankfurt.de>
2012-09-24 10:34:49 +0200

    1 Land mehr (Bosnien-Herzegowina, Umbau Riedwiese)

--- entscripten.pl
+++ entscripten.pl
@@ -4,6 +4,7 @@ $script = 0;
 $g_ausbau = 0;
 $g_klick = 0;
 $g_kilo = 0;
+$g_bb = 0;
 
 while( <> ) {
  $script = 1 if /<script\b/;
@@ -15,12 +16,16 @@ while( <> ) {
  $g_klick++ if /<g/ && $g_klick > 0;
  $g_klick++  if /<g id="clickrechtecke"/;
 
+ $g_bb++ if /<g/ && $g_bb > 0;
+ $g_bb++  if /<g id="bbrechtecke"/;
+
  $g_kilo++ if /<g/ && $g_kilo > 0;
  $g_kilo++  if /<g id="kilometrierungen"/;
 
- print unless $script > 0 || $g_ausbau > 0 || $g_kilo > 0 || $g_klick > 0;
+ print unless $script > 0 || $g_ausbau > 0 || $g_kilo > 0 || $g_klick > 0 || $g_bb > 0;
  $script = 0 if /<\/script>/;
  $g_kilo-- if /<\/g/ && $g_kilo > 0;
  $g_klick-- if /<\/g/ && $g_klick > 0;
  $g_ausbau-- if /<\/g/ && $g_ausbau > 0;
+ $g_bb-- if /<\/g/ && $g_bb > 0;
 }
