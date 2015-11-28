8d2ec5fcf793d779c7e3f99862be214b477e2006
Bjørn Bäuchle <baeuchle@frankfurtium.de>
2015-11-28 08:00:25 +0100

    U-Bahn: Preungesheim umgebaut (*), distribute.sh
    *: Preungesheim: Ab nächstem Montag (heute ist Samstag).
    distribute.sh macht Dinge für public_html nur noch, wenn es das gibt.
    
    Signed-off-by: Bjørn Bäuchle <baeuchle@frankfurtium.de>

--- distribute.sh
+++ distribute.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-echo "1024"
-convert -resize 1024x454 countries-ive-been-to.svg countries-ive-been-to-1024.png
-echo "512"
-convert -resize  512x227 countries-ive-been-to.svg countries-ive-been-to-512.png
-echo "256"
-convert -resize  256x113 countries-ive-been-to.svg countries-ive-been-to-256.png
-echo "128"
-convert -resize  128x57  countries-ive-been-to.svg countries-ive-been-to-128.png
-echo "Moving countries"
-mv countries-ive-been-to*.png ~/public_html/bilder/
-cp countries-ive-been-to.svg  ~/public_html/bilder/
 
 echo "Update date"
 for i in straßenbahnnetz.svg ubahnnetz.svg; do
@@ -19,9 +8,22 @@ for i in straßenbahnnetz.svg ubahnnetz.svg; do
     perl -pi -e '@f = localtime(time); $_ =~ s/2011-\d{4}/sprintf "2011-%4d", $f[5]+1900/gex' $i
 done
 
-echo "Moving Netzplaene"
-cp ubahnnetz.svg straßenbahnnetz.svg ~/public_html/images/
-
-echo "Entscripten"
-./entscripten.pl < ubahnnetz.svg > ubahnnetz.wiki.svg
-./entscripten.pl < straßenbahnnetz.svg > straßenbahnnetz.wiki.svg
+if [ -d ~/public_html/images ]; then
+    echo "1024"
+    convert -resize 1024x454 countries-ive-been-to.svg countries-ive-been-to-1024.png
+    echo "512"
+    convert -resize  512x227 countries-ive-been-to.svg countries-ive-been-to-512.png
+    echo "256"
+    convert -resize  256x113 countries-ive-been-to.svg countries-ive-been-to-256.png
+    echo "128"
+    convert -resize  128x57  countries-ive-been-to.svg countries-ive-been-to-128.png
+    echo "Moving countries"
+    mv countries-ive-been-to*.png ~/public_html/bilder/
+    cp countries-ive-been-to.svg  ~/public_html/bilder/
+    echo "Moving Netzplaene"
+    cp ubahnnetz.svg straßenbahnnetz.svg ~/public_html/images/
+    
+    echo "Entscripten"
+    ./entscripten.pl < ubahnnetz.svg > ubahnnetz.wiki.svg
+    ./entscripten.pl < straßenbahnnetz.svg > straßenbahnnetz.wiki.svg
+fi
