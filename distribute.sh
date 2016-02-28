06a51eb81a62dac9403e81b5d2d4c506ea2bf085
Bjørn Bäuchle <baeuchle@frankfurtium.de>
2016-02-28 14:26:06 +0100

    Umzug auf plan.frankfurtium.de
    Signed-off-by: Bjørn Bäuchle <baeuchle@frankfurtium.de>

--- distribute.sh
+++ distribute.sh
@@ -8,20 +8,23 @@ for i in straßenbahnnetz.svg ubahnnetz.svg; do
     perl -pi -e '@f = localtime(time); $_ =~ s/2011-\d{4}/sprintf "2011-%4d", $f[5]+1900/gex' $i
 done
 
-if [ -d ~/public_html/images ]; then
-    echo "1024"
-    convert -resize 1024x454 countries-ive-been-to.svg countries-ive-been-to-1024.png
-    echo "512"
-    convert -resize  512x227 countries-ive-been-to.svg countries-ive-been-to-512.png
-    echo "256"
-    convert -resize  256x113 countries-ive-been-to.svg countries-ive-been-to-256.png
-    echo "128"
-    convert -resize  128x57  countries-ive-been-to.svg countries-ive-been-to-128.png
-    echo "Moving countries"
-    mv countries-ive-been-to*.png ~/public_html/bilder/
-    cp countries-ive-been-to.svg  ~/public_html/bilder/
+if [ -d /var/www/plan ]; then
+    if [ $(which convert) ]; then
+        echo "1024"
+        convert -resize 1024x454 countries-ive-been-to.svg countries-ive-been-to-1024.png
+        echo "512"
+        convert -resize  512x227 countries-ive-been-to.svg countries-ive-been-to-512.png
+        echo "256"
+        convert -resize  256x113 countries-ive-been-to.svg countries-ive-been-to-256.png
+        echo "128"
+        convert -resize  128x57  countries-ive-been-to.svg countries-ive-been-to-128.png
+        echo "Moving countries"
+        mv countries-ive-been-to*.png /var/www/plan/
+    fi
+    cp countries-ive-been-to.svg  /var/www/plan/
     echo "Moving Netzplaene"
-    cp ubahnnetz.svg straßenbahnnetz.svg ~/public_html/images/
+    cp ubahnnetz.svg /var/www/plan/
+    cp straßenbahnnetz.svg /var/www/plan/strassenbahnnetz.svg
     
 fi
 echo "Entscripten"
