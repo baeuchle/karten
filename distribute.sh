1c97c46227556356323fa2ee5997dc6c8ba66281
Bjørn Bäuchle <baeuchle@frankfurtium.de>
2016-06-12 20:34:08 +0200

    Revision tags in Gleisplänen
    Signed-off-by: Bjørn Bäuchle <baeuchle@frankfurtium.de>

--- distribute.sh
+++ distribute.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+revision=$(git log -1 --pretty=format:%H)
 
-echo "Update date"
+echo "Update date and version ($revision)"
 for i in straßenbahnnetz.svg ubahnnetz.svg; do
-    perl -pi -e '@f = localtime(time); $_ =~ s/>.+</sprintf ">%04d-%02d-%02d<", $f[5]+1900, $f[4]+1, $f[3]/gex if /dc:date/' $i
-    perl -pi -e '@f = localtime(time); @a = qw ! Januar Februar März April Mai Juni Juli August September Oktober November Dezember !; $_ =~ s/Stand: .+?</sprintf "Stand: %s %4d<", $a[$f[4]], $f[5]+1900/gex' $i
-    perl -pi -e '@f = localtime(time); $_ =~ s/2011-\d{4}/sprintf "2011-%4d", $f[5]+1900/gex' $i
+    cp $i $i.dist
+    perl -pi -e '@f = localtime(time); $_ =~ s/>.+</sprintf ">%04d-%02d-%02d<", $f[5]+1900, $f[4]+1, $f[3]/gex if /dc:date/' $i.dist
+    perl -pi -e '@f = localtime(time); @a = qw ! Januar Februar März April Mai Juni Juli August September Oktober November Dezember !; $_ =~ s/Stand: .+?</sprintf "Stand: %s %4d<", $a[$f[4]], $f[5]+1900/gex' $i.dist
+    perl -pi -e '@f = localtime(time); $_ =~ s/2011-\d{4}/sprintf "2011-%4d", $f[5]+1900/gex' $i.dist
+    perl -pi -e "s/rev=[\da-f]{40}/rev=$revision/" $i.dist
+    perl -pi -e "s/\([\da-f]{40}\)/($revision)/" $i.dist
 done
+
+exit;
 
 if [ -d /var/www/plan ]; then
     if [ $(which convert) ]; then
@@ -23,11 +29,11 @@ if [ -d /var/www/plan ]; then
     fi
     cp countries-ive-been-to.svg  /var/www/plan/
     echo "Moving Netzplaene"
-    cp ubahnnetz.svg /var/www/plan/
-    cp straßenbahnnetz.svg /var/www/plan/strassenbahnnetz.svg
+    cp ubahnnetz.svg.dist /var/www/plan/ubahnnetz.svg
+    cp straßenbahnnetz.svg.dist /var/www/plan/strassenbahnnetz.svg
     cp fahrrad.svg /var/www/plan/
     cp oldrev.pl /var/www/plan/
 fi
 echo "Entscripten"
-./entscripten.pl < ubahnnetz.svg > ubahnnetz.wiki.svg
-./entscripten.pl < straßenbahnnetz.svg > straßenbahnnetz.wiki.svg
+./entscripten.pl < ubahnnetz.svg.dist > ubahnnetz.wiki.svg
+./entscripten.pl < straßenbahnnetz.svg.dist > straßenbahnnetz.wiki.svg
