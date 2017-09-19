41f76b672ed4b34820854cf1789c1de128e83f57
Bjørn Bäuchle <baeuchle@frankfurtium.de>
2017-09-19 15:33:11 +0200

    U-Bahn: Riedberg, Europaviertel, Legende
    - Riedberg:
      Der Hinweis auf das U2-Verbot ist gelöscht (keine U2 mehr im Betrieb)
    - Europaviertel:
      Die Strecke zum Europaviertel wird nun gestrichelt eingezeichnet, weil
      in Bau
    - Legende:
      Die Legende enthält nun einen Hinweis auf im Bau befindliche
      Abschnitte.
    
    Signed-off-by: Bjørn Bäuchle <baeuchle@frankfurtium.de>

--- entscripten.pl
+++ entscripten.pl
@@ -22,16 +22,19 @@ while (<>) {
   $g_bb++  if /<g id="bbrechtecke"/;
   $g_kilo++  if /<g id="kilometrierungen"/;
   $g_klick++  if /<g id="clickrechtecke"/;
-  $g_wiki++  if /<g class="nowiki"/;
+  $g_wiki++  if /<(g|tspan) class="nowiki"/;
  
   print unless $script > 0 || $g_ausbau > 0 || $g_kilo > 0 || $g_klick > 0 || $g_bb > 0 || $g_wiki > 0;
 
   $script = 0 if m{</script};
   if (m{</g}) {
-  $g_ausbau-- if $g_ausbau > 0;
+    $g_ausbau-- if $g_ausbau > 0;
     $g_bb-- if $g_bb > 0;
     $g_kilo-- if $g_kilo > 0;
     $g_klick-- if $g_klick > 0;
+    $g_wiki-- if $g_wiki > 0;
+  }
+  if (m{</tspan}) {
     $g_wiki-- if $g_wiki > 0;
   }
 }
