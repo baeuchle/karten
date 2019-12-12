da430ef22a50487531626b2446b680c94c7b13f0
Bjørn Bäuchle <baeuchle@frankfurtium.de>
2019-12-12 22:23:42 +0100

    FIX entscripten extended and adapted

--- entscripten.pl
+++ entscripten.pl
@@ -6,12 +6,14 @@ my $g_bb = 0;
 my $g_klick = 0;
 my $g_kilo = 0;
 my $g_wiki = 0;
+my $g_always_links = 0;
 
 while (<>) {
   $script = 1 if /<script\b/;
   s/onclick="[^"]+"//g;
   s/onload="[^"]+"//g;
   s/onmouseover="[^"]+"//g;
+  s/onmousemove="[^"]+"//g;
   s/\.nowiki\s+\{\s+display\:inline/.nowiki \{ display:none/g;
   next if /Dieser Plan bei Wikipedia/;
   next if /de.wikipedia.org.favicon/;
@@ -22,14 +24,16 @@ while (<>) {
     $g_kilo++ if /<g/ && $g_kilo > 0;
     $g_klick++ if /<g/ && $g_klick > 0;
     $g_wiki++ if /<g/ && $g_wiki > 0;
+    $g_always_links++ if /<g/ && $g_always_links > 0;
   }
   $g_ausbau++  if /<g id="ausbauprojekte"/;
   $g_bb++  if /<g id="bbrechtecke"/;
   $g_kilo++  if /<g id="kilometrierungen"/;
   $g_klick++  if /<g id="clickrechtecke"/;
   $g_wiki++  if /<(g|tspan) class="nowiki"/;
+  $g_always_links++ if /<g[^>]*\sid="always_links"/;
  
-  print unless $script > 0 || $g_ausbau > 0 || $g_kilo > 0 || $g_klick > 0 || $g_bb > 0 || $g_wiki > 0;
+  print unless $script > 0 || $g_ausbau > 0 || $g_kilo > 0 || $g_klick > 0 || $g_bb > 0 || $g_wiki > 0 || $g_always_links >  0;
 
   $script = 0 if m{</script};
   if (m{</g}) {
@@ -38,6 +42,7 @@ while (<>) {
     $g_kilo-- if $g_kilo > 0;
     $g_klick-- if $g_klick > 0;
     $g_wiki-- if $g_wiki > 0;
+    $g_always_links-- if $g_always_links > 0;
   }
   if (m{</tspan}) {
     $g_wiki-- if $g_wiki > 0;
